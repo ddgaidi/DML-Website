90624ac4c47f5af44c430e7d8df48df14bf23efa
DML <90888178+ddgaidi@users.noreply.github.com>
2024-11-10 23:40:53 +0100

    code push

--- src/lib/components/navbar/Navbar.svelte
+++ src/lib/components/navbar/Navbar.svelte
@@ -8,33 +8,39 @@
   import tradb from '../../images/SVG/navbar/tradb.svg';
   import tradw from '../../images/SVG/navbar/tradw.svg';
 
+  let isMobileMenuOpen = false;
+
   const toggleTheme = () => {
     darkMode.update(value => !value);
   };
 
-  function toggleLanguage() {
+  const toggleLanguage = () => {
     lang.update(value => {
       const newLang = value === 'fr' ? 'en' : 'fr';
       saveLanguageChoice(newLang);
       return newLang;
     });
-  }
+  };
 
-  function saveUserChoice(isDarkModeEnabled: boolean) {
+  const saveUserChoice = (isDarkModeEnabled: boolean) => {
     const cookie = {
       name: "darkMode",
       value: isDarkModeEnabled ? "enabled" : "disabled"
     };
     domCookie.set(cookie);
-  }
+  };
 
-  function saveLanguageChoice(selectedLang: string) {
+  const saveLanguageChoice = (selectedLang: string) => {
     const cookie = {
       name: "language",
       value: selectedLang
     };
     domCookie.set(cookie);
-  }
+  };
+
+  const toggleMobileMenu = () => {
+    isMobileMenuOpen = !isMobileMenuOpen;
+  };
 
   onMount(() => {
     const themeCookie = domCookie.get("darkMode");
@@ -56,30 +62,75 @@
   });
 </script>
 
-<div class="fixed flex items-center justify-center w-full pt-5 z-[9999]" id="navbar">
-  <div class="transition-all duration-500 group bg-[#d4d5e2] dark:bg-[#14141E] flex justify-center items-center cursor-pointer rounded-[20px] w-[53.5px] h-[53.5px] border-2 border-solid border-[#aaabbb] dark:border-[#868796] hover:bg-[#bdbeca] dark:hover:bg-[#23232e] hover:border-2 hover:border-[#9697a5] dark:hover:border-[#b5b6c9]" id="theme" on:click={toggleTheme} role="button" tabindex="0" on:keydown={e => { if (e.key === 'Enter') toggleTheme(); }}>
-    <div class="mx-[10px] my-[10px]">
-      <img src={sun} alt="HTML Noir" class="absolute top-[34px] left-[563px] transition-opacity duration-500 ease-in-out h-[25px] {$darkMode ? 'opacity-100' : 'opacity-0'}" />
-      <img src={moon} alt="HTML Blanc" class="absolute top-[34px] left-[564px] transition-opacity duration-500 ease-in-out h-[25px] {$darkMode ? 'opacity-0' : 'opacity-100'}" />
+<div class="fixed w-full pt-5 z-[9999]" id="navbar">
+  <!-- Navbar pour Desktop -->
+  <div class="hidden lg:flex items-center justify-center">
+    <!-- Bouton de thème -->
+    <div class="transition-all duration-500 group bg-[#d4d5e2] dark:bg-[#14141E] flex justify-center items-center cursor-pointer rounded-[20px] w-[53.5px] h-[53.5px] border-2 border-solid border-[#aaabbb] dark:border-[#868796] hover:bg-[#bdbeca] dark:hover:bg-[#23232e] hover:border-2 hover:border-[#9697a5] dark:hover:border-[#b5b6c9]" id="theme" on:click={toggleTheme} role="button" tabindex="0" on:keydown={e => { if (e.key === 'Enter') toggleTheme(); }}>
+      <div class="mx-[10px] my-[10px]">
+        <img src={sun} alt="HTML Noir" class="absolute top-[34px] left-[563px] transition-opacity duration-500 ease-in-out h-[25px] {$darkMode ? 'opacity-100' : 'opacity-0'}" />
+        <img src={moon} alt="HTML Blanc" class="absolute top-[34px] left-[564px] transition-opacity duration-500 ease-in-out h-[25px] {$darkMode ? 'opacity-0' : 'opacity-100'}" />
+      </div>
+    </div>
+
+    <!-- Liens de page -->
+    <div class="transition-colors duration-500 bg-[#d4d5e2] dark:bg-[#14141E] flex justify-between w-[290px] h-[53.5px] mx-3 px-1 py-1 rounded-[15px] border-2 border-solid border-[#aaabbb] dark:border-[#868796]" id="page">
+      {#if $lang === 'fr'}
+        <a class="transition-colors duration-500 text-[#5e5e5e] dark:text-[#a2a4b3] text-[15px] m-2.5 hover:text-[#000000] dark:hover:text-[#ffffff]" href="/">/Qui suis-je</a>
+        <a class="transition-colors duration-500 text-[#5e5e5e] dark:text-[#a2a4b3] text-[15px] m-2.5 hover:text-[#000000] dark:hover:text-[#ffffff]" href="/creations">/Créations</a>
+        <a class="transition-colors duration-500 text-[#5e5e5e] dark:text-[#a2a4b3] text-[15px] m-2.5 hover:text-[#000000] dark:hover:text-[#ffffff]" href="/contact">/Contact</a>
+      {:else}
+        <a class="transition-colors duration-500 text-[#5e5e5e] dark:text-[#a2a4b3] text-[15px] m-2.5 hover:text-[#000000] dark:hover:text-[#ffffff]" href="/">/About Me</a>
+        <a class="transition-colors duration-500 text-[#5e5e5e] dark:text-[#a2a4b3] text-[15px] m-2.5 hover:text-[#000000] dark:hover:text-[#ffffff]" href="/creations">/Creations</a>
+        <a class="transition-colors duration-500 text-[#5e5e5e] dark:text-[#a2a4b3] text-[15px] m-2.5 hover:text-[#000000] dark:hover:text-[#ffffff]" href="/contact">/Contact</a>
+      {/if}
+    </div>
+
+    <!-- Bouton de langue -->
+    <div class="transition-colors duration-500 group bg-[#d4d5e2] dark:bg-[#14141E] flex justify-center items-center cursor-pointer rounded-[20px] w-[53.5px] h-[53.5px] border-2 border-solid border-[#aaabbb] dark:border-[#868796] hover:bg-[#bdbeca] dark:hover:bg-[#23232e] hover:border-2 hover:border-[#9697a5] dark:hover:border-[#b5b6c9]" id="langue" on:click={toggleLanguage} role="button" tabindex="0" on:keydown={e => { if (e.key === 'Enter' || e.key === ' ') toggleLanguage(); }}>
+      <button class="mx-[10px] my-[10px]">
+        <img src={tradb} alt="Traduction Sombre" class="absolute top-[34px] right-[563px] transition-opacity duration-500 ease-in-out h-[25px] {$darkMode ? 'opacity-100' : 'opacity-0'}" />
+        <img src={tradw} alt="Traduction Clair" class="absolute top-[34px] right-[564px] transition-opacity duration-500 ease-in-out h-[25px] {$darkMode ? 'opacity-0' : 'opacity-100'}" />
+      </button>
     </div>
   </div>
 
-  <div class="transition-colors duration-500 bg-[#d4d5e2] dark:bg-[#14141E] flex justify-between w-[290px] h-[53.5px] mx-3 px-1 py-1 rounded-[15px] border-2 border-solid border-[#aaabbb] dark:border-[#868796]" id="page">
-    {#if $lang === 'fr'}
-      <a class="transition-colors duration-500 text-[#5e5e5e] dark:text-[#a2a4b3] text-[15px] m-2.5 hover:text-[#000000] dark:hover:text-[#ffffff]" href="/">/Qui suis-je</a>
-      <a class="transition-colors duration-500 text-[#5e5e5e] dark:text-[#a2a4b3] text-[15px] m-2.5 hover:text-[#000000] dark:hover:text-[#ffffff]" href="/creations">/Créations</a>
-      <a class="transition-colors duration-500 text-[#5e5e5e] dark:text-[#a2a4b3] text-[15px] m-2.5 hover:text-[#000000] dark:hover:text-[#ffffff]" href="/contact">/Contact</a>
-    {:else}
-      <a class="transition-colors duration-500 text-[#5e5e5e] dark:text-[#a2a4b3] text-[15px] m-2.5 hover:text-[#000000] dark:hover:text-[#ffffff]" href="/">/About Me</a>
-      <a class="transition-colors duration-500 text-[#5e5e5e] dark:text-[#a2a4b3] text-[15px] m-2.5 hover:text-[#000000] dark:hover:text-[#ffffff]" href="/creations">/Creations</a>
-      <a class="transition-colors duration-500 text-[#5e5e5e] dark:text-[#a2a4b3] text-[15px] m-2.5 hover:text-[#000000] dark:hover:text-[#ffffff]" href="/contact">/Contact</a>
+  <!-- Navbar pour Mobile -->
+  <div class="lg:hidden flex items-center px-3 py-3 mt-[-20px]  bg-[#edebf5] dark:bg-[#0a0a0f]">
+    <!-- Bouton Hamburger pour ouvrir le menu -->
+    <button class="text-[#5e5e5e] dark:text-[#a2a4b3]" on:click={toggleMobileMenu}>
+      <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+      </svg>
+    </button>
+  
+    <!-- Bouton pour changer de thème -->
+    <button class="ml-3 mb-6" on:click={toggleTheme}>
+      <img src={sun} alt="Mode clair" class="transition-opacity duration-500 h-6 absolute {$darkMode ? 'opacity-100' : 'opacity-0'}" />
+      <img src={moon} alt="Mode sombre" class="transition-opacity duration-500 h-6 absolute {$darkMode ? 'opacity-0' : 'opacity-100'}" />
+    </button>
+  
+    <!-- Bouton pour changer la langue -->
+    <button class="ml-9 mb-6" on:click={toggleLanguage}>
+      <img src={tradb} alt="Langue sombre" class="transition-opacity duration-500 h-6 absolute {$darkMode ? 'opacity-100' : 'opacity-0'}" />
+      <img src={tradw} alt="Langue claire" class="transition-opacity duration-500 h-6 absolute {$darkMode ? 'opacity-0' : 'opacity-100'}" />
+    </button>
+  
+    <!-- Menu déroulant pour la navigation -->
+    {#if isMobileMenuOpen}
+      <div class="absolute top-14 left-0 w-full  bg-[#e4e3ed] dark:bg-[#07070b] p-5 flex flex-col space-y-4">
+        <!-- Liens de navigation -->
+        {#if $lang === 'fr'}
+          <a class="text-[#5e5e5e] dark:text-[#a2a4b3] text-lg hover:text-[#000000] dark:hover:text-[#ffffff]" href="/" on:click={toggleMobileMenu}>/Qui suis-je</a>
+          <a class="text-[#5e5e5e] dark:text-[#a2a4b3] text-lg hover:text-[#000000] dark:hover:text-[#ffffff]" href="/creations" on:click={toggleMobileMenu}>/Créations</a>
+          <a class="text-[#5e5e5e] dark:text-[#a2a4b3] text-lg hover:text-[#000000] dark:hover:text-[#ffffff]" href="/contact" on:click={toggleMobileMenu}>/Contact</a>
+        {:else}
+          <a class="text-[#5e5e5e] dark:text-[#a2a4b3] text-lg hover:text-[#000000] dark:hover:text-[#ffffff]" href="/" on:click={toggleMobileMenu}>/About Me</a>
+          <a class="text-[#5e5e5e] dark:text-[#a2a4b3] text-lg hover:text-[#000000] dark:hover:text-[#ffffff]" href="/creations" on:click={toggleMobileMenu}>/Creations</a>
+          <a class="text-[#5e5e5e] dark:text-[#a2a4b3] text-lg hover:text-[#000000] dark:hover:text-[#ffffff]" href="/contact" on:click={toggleMobileMenu}>/Contact</a>
+        {/if}
+      </div>
     {/if}
   </div>
-
-  <div class="transition-colors duration-500 group bg-[#d4d5e2] dark:bg-[#14141E] flex justify-center items-center cursor-pointer rounded-[20px] w-[53.5px] h-[53.5px] border-2 border-solid border-[#aaabbb] dark:border-[#868796] hover:bg-[#bdbeca] dark:hover:bg-[#23232e] hover:border-2 hover:border-[#9697a5] dark:hover:border-[#b5b6c9]" id="langue" on:click={toggleLanguage} role="button" tabindex="0" on:keydown={e => { if (e.key === 'Enter' || e.key === ' ') toggleLanguage(); }}>
-    <button class="mx-[10px] my-[10px]">
-      <img src={tradb} alt="Traduction Sombre" class="absolute top-[34px] right-[563px] transition-opacity duration-500 ease-in-out h-[25px] {$darkMode ? 'opacity-100' : 'opacity-0'}" />
-      <img src={tradw} alt="Traduction Clair" class="absolute top-[34px] right-[564px] transition-opacity duration-500 ease-in-out h-[25px] {$darkMode ? 'opacity-0' : 'opacity-100'}" />
-    </button>
-  </div>
+  
 </div>
--- src/lib/components/qui/bio/Bio.svelte
+++ src/lib/components/qui/bio/Bio.svelte
@@ -3,22 +3,24 @@
   import pdp from '../../../images/IMG/pdp/pdp.png';
 </script>
 
-<div class="transition-colors duration-500 pt-[100px] pb-[10px] bg-[#f4f3fc] dark:bg-[#0C0C12] flex justify-center items-center" id="biographie">
+<div class="transition-colors duration-500 lg:pt-[90px] pt-[70px] bg-[#f4f3fc] dark:bg-[#0C0C12] flex justify-center items-center" id="biographie">
 
-  <div class="transition-colors duration-500 bg-[#d4d5e2] dark:bg-[#14141E] flex items-center w-[800px] p-5 rounded-[10px]" id="bio-container">
-
-    <div class="mr-7" id="pdp">
-      <img class="w-[600px] rounded-[40px]" src={pdp} alt="Photo_de_profil">
+  <div class="transition-colors duration-500 bg-[#d4d5e2] dark:bg-[#14141E] flex flex-col lg:flex-row items-center w-[90%] lg:max-w-[800px] p-4 lg:p-5 rounded-[10px]" id="bio-container">
+    
+    <!-- Photo de profil -->
+    <div class="mb-4 lg:mb-0 lg:mr-7 w-full lg:w-auto flex justify-center" id="pdp">
+      <img class="w-[40%] lg:w-[300px] rounded-[20px] lg:rounded-[40px]" src={pdp} alt="Photo_de_profil">
     </div>
-
-    <div class="transition-colors duration-500 flex flex-col" id="bio-text">
-      <h1 class="transition-colors duration-500 text-[40px] text-black dark:text-[#c2c4d4] mb-[10px] font-family: 'Outfit', sans-serif dark:text-white font-black">
+    
+    <!-- Texte biographique -->
+    <div class="transition-colors duration-500 flex flex-col items-center lg:items-start text-justify lg:text-left" id="bio-text">
+      <h1 class="transition-colors duration-500 text-[18px] lg:text-[40px] text-black dark:text-[#c2c4d4] mb-[10px] font-family: 'Outfit', sans-serif dark:text-white font-black">
         <span class="transition-colors duration-500 font-extrabold text-[#ff9900] dark:text-[#FFC000]">DML</span> | 
-        <span class="transition-colors duration-500 font-semibold text-[#4e59d8] dark:text-[#5865f2]"> 
+        <span class="transition-colors duration-500 font-semibold text-[#4e59d8] dark:text-[#5865f2]">
           {$lang === 'fr' ? "Djamel Dyne GAÏDI" : "Djamel Dyne GAÏDI"}
         </span>
       </h1>
-      <p class="transition-colors duration-500 text-[17px] w-[600px] text-black dark:text-[#868894] font-family: 'Outfit', sans-serif dark:text-white">
+      <p class="transition-colors duration-500 text-[12px] lg:text-[17px] max-w-[99%] lg:w-[600px] text-black dark:text-[#868894] font-family: 'Outfit', sans-serif dark:text-white">
         {$lang === 'fr' 
           ? "Je m'appelle Djamel, j'ai 18 ans et je réside en région parisienne. Depuis mon plus jeune âge, ma passion pour l'informatique ne cesse de grandir. Au cours des dernières années, j'ai particulièrement plongé dans le domaine du développement web et je me suis concentré sur l'apprentissage approfondi du framework Svelte." 
           : "My name is Djamel, I am 18 years old, and I live in the beautiful Paris region. From a young age, my passion for computing has continued to grow steadily. Over the last few years, I have particularly delved into web development and focused on an in-depth learning of the Svelte framework, which I find fascinating and powerful."}
